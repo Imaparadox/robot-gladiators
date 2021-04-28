cfc53f9e87a7f4f3e4138517279a80dddcf2334b
Stephen Marsh <stevemarsh117@gmail.com>
2021-04-28 11:48:43 -0400

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,12 +5,12 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 //Enemy
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 //Fight Function
-var fight = function () {
+var fight = function (enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -56,30 +56,16 @@ var fight = function () {
         fight();
     }
 
-    // //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-    // enemyHealth = enemyHealth - playerAttack;
-    // // Log a resulting message to the console so we know that it worked.
-    // console.log(
-    //     playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.")
-    // // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-    // playerHealth = playerHealth - enemyAttack;
-    // // Log a resulting message to the console so we know that it worked.
-    // console.log(
-    //     enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.")
-    // // check player's health
-    // if (playerHealth <= 0) {
-    //     window.alert(playerName + " has died!");
-    // }
-    // else {
-    //     window.alert(playerName + " still has " + playerHealth + " health left.");
-    // }
-    // // check enemy's health
-    // if (enemyHealth <= 0) {
-    //     window.alert(enemyName + " has died!");
-    // }
-    // else {
-    //     window.alert(enemyName + " still has " + enemyHealth + " health left.");
-    // }
 };
 
-fight();
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+
+//fight();
+
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
